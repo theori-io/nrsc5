259b0b7d515b74b91f3dd7687ea42b51b7f0d07f
Ian Bobbitt <ian@icb.im>
2017-06-13 08:20:50 -0400

    Adding Homebrew Formula and instructions.

---
+++ nrsc5.rb
@@ -0,0 +1,20 @@
+class Nrsc5 < Formula
+  desc "NRSC-5 receiver for rtl-sdr"
+  homepage "http://theori.io/research/nrsc-5-c"
+  head "https://github.com/theori-io/nrsc5.git"
+
+  depends_on "cmake" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "librtlsdr"
+  depends_on "libao"
+  depends_on "fftw"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+end

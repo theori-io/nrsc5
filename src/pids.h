5d01022fd5746eac4442877707b9feab83a3a410
Clayton Smith <argilo@gmail.com>
2017-09-06 08:44:20 -0400

    Only log SIS messages when there's a change.

--- src/pids.h
+++ src/pids.h
@@ -2,4 +2,12 @@
 
 #include <stdint.h>
 
-void pids_frame_push(uint8_t *bits);
+typedef struct
+{
+    char country_code[3];
+    int fcc_facility_id;
+    char short_name[8];
+} pids_t;
+
+void pids_frame_push(pids_t *st, uint8_t *bits);
+void pids_init(pids_t *st);

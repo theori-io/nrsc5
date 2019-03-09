a40277e2686ea59a1a61d573b521c7adf8c9bfb5
Andrew Wesie <awesie@gmail.com>
2019-03-10 00:54:54 +0900

    Add simple keyboard commands to CLI. (#197)
    * Add simple keyboard commands to CLI.
    
    Resolves #73.
    
    * Use _getch on Windows.
    
    * Use read() instead of getchar().
    
    This seems to fix issues with pthread_cancel not interrupting getchar.
    
    * Make CLI interactive only if stdin is a tty.
    
    * Stop the radio thread if user wants to quit.

--- src/main.c
+++ src/main.c
@@ -19,13 +19,17 @@
 #include <nrsc5.h>
 #include <pthread.h>
 #include <sys/time.h>
+#include <unistd.h>
 
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
 
 #ifdef __MINGW32__
+#include <conio.h>
 #include <windows.h>
+#else
+#include <termios.h>
 #endif
 
 #include "bitwriter.h"
@@ -99,7 +103,7 @@ static void reset_audio_buffers(state_t *st)
     st->tail = NULL;
 }
 
-static void push_audio_buffer(state_t *st, const int16_t *data, size_t count)
+static void push_audio_buffer(state_t *st, unsigned int program, const int16_t *data, size_t count)
 {
     audio_buffer_t *b;
     struct timespec ts;
@@ -115,6 +119,9 @@ static void push_audio_buffer(state_t *st, const int16_t *data, size_t count)
     }
 
     pthread_mutex_lock(&st->mutex);
+    if (program != st->program)
+        goto unlock;
+
     while (st->free == NULL)
     {
         if (pthread_cond_timedwait(&st->cond, &st->mutex, &ts) == ETIMEDOUT)
@@ -131,6 +138,13 @@ static void push_audio_buffer(state_t *st, const int16_t *data, size_t count)
     memcpy(b->data, data, count * sizeof(data[0]));
 
     pthread_mutex_lock(&st->mutex);
+    if (program != st->program)
+    {
+        b->next = st->free;
+        st->free = b;
+        goto unlock;
+    }
+
     b->next = NULL;
     if (st->tail)
         st->tail->next = b;
@@ -142,6 +156,8 @@ static void push_audio_buffer(state_t *st, const int16_t *data, size_t count)
         st->audio_ready++;
 
     pthread_cond_signal(&st->cond);
+
+unlock:
     pthread_mutex_unlock(&st->mutex);
 }
 
@@ -225,6 +241,32 @@ static void dump_ber(float cber)
     log_info("BER: %f, avg: %f, min: %f, max: %f", cber, sum / count, min, max);
 }
 
+static void done_signal(state_t *st)
+{
+    pthread_mutex_lock(&st->mutex);
+    st->done = 1;
+    pthread_cond_signal(&st->cond);
+    pthread_mutex_unlock(&st->mutex);
+}
+
+static void change_program(state_t *st, unsigned int program)
+{
+    pthread_mutex_lock(&st->mutex);
+
+    // reset audio buffers
+    st->audio_ready = 0;
+    if (st->tail)
+    {
+        st->tail->next = st->free;
+        st->free = st->head;
+        st->head = st->tail = NULL;
+    }
+    // update current program
+    st->program = program;
+
+    pthread_mutex_unlock(&st->mutex);
+}
+
 static void callback(const nrsc5_event_t *evt, void *opaque)
 {
     state_t *st = opaque;
@@ -236,10 +278,7 @@ static void callback(const nrsc5_event_t *evt, void *opaque)
     switch (evt->event)
     {
     case NRSC5_EVENT_LOST_DEVICE:
-        pthread_mutex_lock(&st->mutex);
-        st->done = 1;
-        pthread_cond_signal(&st->cond);
-        pthread_mutex_unlock(&st->mutex);
+        done_signal(st);
         break;
     case NRSC5_EVENT_BER:
         dump_ber(evt->ber.cber);
@@ -267,8 +306,7 @@ static void callback(const nrsc5_event_t *evt, void *opaque)
         }
         break;
     case NRSC5_EVENT_AUDIO:
-        if (evt->audio.program == st->program)
-            push_audio_buffer(st, evt->audio.data, evt->audio.count);
+        push_audio_buffer(st, evt->audio.program, evt->audio.data, evt->audio.count);
         break;
     case NRSC5_EVENT_SYNC:
         log_info("Synchronized");
@@ -345,6 +383,66 @@ static void callback(const nrsc5_event_t *evt, void *opaque)
                      data_service->type, data_service->mime_type);
         break;
     }
+}
+
+#ifndef __MINGW32__
+static void restore_termios(void *arg)
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, arg);
+}
+#endif
+
+static void *input_main(void *arg)
+{
+    state_t *st = arg;
+
+    if (!isatty(STDIN_FILENO))
+        return NULL;
+
+#ifndef __MINGW32__
+    struct termios prev_termios, t;
+
+    // disable terminal canonical mode
+    tcgetattr(STDIN_FILENO, &prev_termios);
+    pthread_cleanup_push(restore_termios, &prev_termios);
+    t = prev_termios;
+    t.c_lflag &= ~ICANON;
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
+#endif
+
+    while (!st->done)
+    {
+        int ch;
+
+#ifdef __MINGW32__
+        ch = _getch();
+#else
+        if (read(STDIN_FILENO, &ch, 1) != 1)
+            break;
+#endif
+
+        switch (ch)
+        {
+        case 'q':
+            done_signal(st);
+            // user wants to immediately exit, so reset audio buffer
+            change_program(st, -1);
+            break;
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+            change_program(st, ch - '0');
+            break;
+        }
+    }
+
+#ifndef __MINGW32__
+    // restore terminal settings
+    pthread_cleanup_pop(1);
+#endif
+
+    return NULL;
 }
 
 static void help(const char *progname)
@@ -516,6 +614,7 @@ static void cleanup(state_t *st)
 int main(int argc, char *argv[])
 {
     pthread_mutex_t log_mutex;
+    pthread_t input_thread;
     nrsc5_t *radio = NULL;
     state_t *st = calloc(1, sizeof(state_t));
 
@@ -564,6 +663,8 @@ int main(int argc, char *argv[])
     nrsc5_set_callback(radio, callback, st);
     nrsc5_start(radio);
 
+    pthread_create(&input_thread, NULL, input_main, st);
+
     while (1)
     {
         audio_buffer_t *b;
@@ -596,6 +697,10 @@ int main(int argc, char *argv[])
         pthread_mutex_unlock(&st->mutex);
     }
 
+    pthread_cancel(input_thread);
+    pthread_join(input_thread, NULL);
+
+    nrsc5_stop(radio);
     nrsc5_close(radio);
     cleanup(st);
     free(st);

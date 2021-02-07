17ef5074facafab2dcc032705efaf8b567db0b8a
farlies <farlies@gmail.com>
2021-02-07 15:04:54 -0600

    sig struct documentation

--- include/nrsc5.h
+++ include/nrsc5.h
@@ -33,7 +33,7 @@
 #define NRSC5_MIME_HERE_TPEG        0x82F03DFC
 #define NRSC5_MIME_HERE_IMAGE       0xB7F03DFC
 #define NRSC5_MIME_HD_TMC           0xEECB55B6
-#define NRSC5_MIME_HDC              0x4DC66C5A
+#define NRSC5_MIME_HDC              0x4DC66C5A /**< High Defn Coding audio */
 #define NRSC5_MIME_TEXT             0xBB492AAC
 #define NRSC5_MIME_JPEG             0x1E653E9C
 #define NRSC5_MIME_PNG              0x4F328CA0
@@ -58,26 +58,40 @@ enum
     NRSC5_SIG_COMPONENT_DATA
 };
 
+/**  Represent a channel component.
+ *
+ * An element of a linked list that accompanies a nrsc5_sig_service_t
+ * describing a component of a SIG record. This provides further
+ * information about the audio or data in the channel.
+ */
 struct nrsc5_sig_component_t
 {
-    struct nrsc5_sig_component_t *next;
-    uint8_t type;
-    uint8_t id;
+    struct nrsc5_sig_component_t *next;  /**< Pointer to next element or NULL */
+    uint8_t type; /**< NRSC5_SIG_SERVICE_AUDIO or NRSC5_SIG_SERVICE_DATA */
+    uint8_t id;   /**< Component identifier, 0, 1, 2,... */
     union
     {
+        /*! Data service information
+         */
         struct {
-            uint16_t port;
+            uint16_t port;  /**< distinguishes packets for this service */
+            /** e.g. NRSC5_SERVICE_DATA_TYPE_AUDIO_RELATED_DATA */
             uint16_t service_data_type;
-            uint8_t type;
-            uint32_t mime;
+            uint8_t type;   /**< 0 for stream,  1 for packet, 3 for LOT */
+            uint32_t mime;  /**< content, e.g. NRSC5_MIME_STATION_LOGO */
         } data;
+        /*! Audio service information
+         */
         struct {
-            uint8_t port;
-            uint8_t type;
-            uint32_t mime;
+            uint8_t port;   /**< distinguishes packets for this service */
+            uint8_t type;   /**< 0 for stream,  1 for packet, 3 for LOT */
+            uint32_t mime;  /**< content, e.g. NRSC5_MIME_HDC */
         } audio;
     };
 };
+/**
+ * Defines a typename for struct nrsc5_sig_component_t
+ */
 typedef struct nrsc5_sig_component_t nrsc5_sig_component_t;
 
 enum
@@ -86,14 +100,26 @@ enum
     NRSC5_SIG_SERVICE_DATA
 };
 
+/**  Represent Station Information Guide (SIG) records
+ *
+ * Element of a linked list that will accompany an NRSC5_EVENT_SIG type event.
+ * Each cell describes a channel (audio or data), with a name, e.g. "MPS"
+ * is used to indicate main program service, "SPS1" to indicate
+ * supplemental program service 1.  Each service may include a linked
+ * list of type nrsc5_sig_component_t describing its components, which
+ * may include both audio and other data.
+ */
 struct nrsc5_sig_service_t
 {
-    struct nrsc5_sig_service_t *next;
-    uint8_t type;
-    uint16_t number;
-    const char *name;
-    nrsc5_sig_component_t *components;
+    struct nrsc5_sig_service_t *next; /**< Pointer to next element or NULL */
+    uint8_t type;      /**< NRSC5_SIG_SERVICE_AUDIO or NRSC5_SIG_SERVICE_DATA */
+    uint16_t number;   /**< Channel number: 1,2,3,4 */
+    const char *name;  /**< Channel name, e.g. "MPS" or "SPS1" */
+    nrsc5_sig_component_t *components; /**< Head of linked list of components */
 };
+/**
+ * Defines a typename for struct nrsc5_sig_service_t
+ */
 typedef struct nrsc5_sig_service_t nrsc5_sig_service_t;
 
 enum
@@ -155,17 +181,21 @@ enum
 };
 
 /**
- * Station Information Service datagram. This is a linked list element
- * that may point to further audio service elements.
+ * Station Information Service *Audio* service descriptor. This is a
+ * linked list element that may point to further audio service
+ * descriptor elements.  Refer to NRSC-5 document SY_IDD_1020s.
  */
 struct nrsc5_sis_asd_t
 {
-    struct nrsc5_sis_asd_t *next;
-    unsigned int program;
-    unsigned int access;
-    unsigned int type;
-    unsigned int sound_exp;
+    struct nrsc5_sis_asd_t *next; /**< Pointer to next element or NULL */
+    unsigned int program;     /**< program number 0, 1, ..., 7 */
+    unsigned int access;      /**< NRSC5_ACCESS_PUBLIC or NRSC5_ACCESS_RESTRICTED */
+    unsigned int type;        /**< audio service, e.g. NRSC5_PROGRAM_TYPE_JAZZ  */
+    unsigned int sound_exp;   /**< 0 is none, 2 is Dolby Pro Logic II Surround */
 };
+/**
+ * Defines a typename for struct nrsc5_sis_asd_t
+ */
 typedef struct nrsc5_sis_asd_t nrsc5_sis_asd_t;
 
 enum
@@ -191,24 +221,29 @@ enum
 };
 
 /**
- * Station Information Service. This is a linked list element that may point
- * to further data service elements via `next` member if not `NULL`.
- * See nrsc5_service_data_type_name() for named types of data.
+ * Station Information Service *Data* service descriptor. This is a
+ * linked list element that may point to further data service descriptors
+ * via `next` member if not `NULL`.  See
+ * nrsc5_service_data_type_name() for named types of data.
+ * Refer to NRSC-5 document SY_IDD_1020s.
  */
 struct nrsc5_sis_dsd_t
 {
     struct nrsc5_sis_dsd_t *next; /**< Pointer to next element or NULL */
-    unsigned int access;  /**< public or restricted */
-    unsigned int type;    /**< service data type */
-    uint32_t mime_type;   /**< MIME type. See e.g. `NRSC5_MIME_TEXT` */
+    unsigned int access;  /**< NRSC5_ACCESS_PUBLIC or NRSC5_ACCESS_RESTRICTED */
+    unsigned int type; /**< data service type, e.g. NRSC5_SERVICE_DATA_TYPE_TEXT */
+    uint32_t mime_type;   /**< MIME type, e.g. `NRSC5_MIME_TEXT` */
 };
+/**
+ * Defines a typename for struct nrsc5_sis_dsd_t
+ */
 typedef struct nrsc5_sis_dsd_t nrsc5_sis_dsd_t;
 
 /**  Incoming event from receiver.
  *
  * This event structure is passed to your application supplied
- * callback function.  It is a union of various structs, keyed by
- * member `event`.
+ * callback function--see nrsc5_set_callback().  It is a union of
+ * various structs, keyed by member `event`.
  */
 struct nrsc5_event_t
 {
@@ -295,18 +330,28 @@ struct nrsc5_event_t
         } sis;
     };
 };
+/**
+ * Defines a typename for struct nrsc5_event_t
+ */
 typedef struct nrsc5_event_t nrsc5_event_t;
 
+/**
+ * Prototype for a user event handling callback function.
+ * The function will be invoked with a pointer to the nrsc5_event_t
+ * and a pointer to a second contextual argument, established by
+ * nrsc5_set_callback().
+ */
 typedef void (*nrsc5_callback_t)(const nrsc5_event_t *evt, void *opaque);
 
 /**
  * An opaque data type used by API functions to represent session information.
- * Applications should acquire a pointer to one via the "open" functions like
+ * Applications should acquire a pointer to one via the `open_` functions:
  * nrsc5_open(), nrsc5_open_file(), nrsc5_open_pipe(),  or nrsc5_open_rtltcp().
  */
 typedef struct nrsc5_t nrsc5_t;
 
-/*
+
+/* ============================================================================
  * Public functions. All functions return void or an error code (0 == success).
  */
 
@@ -513,8 +558,8 @@ void nrsc5_set_auto_gain(nrsc5_t *st, int enabled);
  * Establish a callback function.
  *
  * @param[in] st  pointer to an `nrsc5_t` session object
- * @param[in] callback  set to an event handling function of two argument
- * @param[in] opaque    set to a pointer to the function's 2nd argument
+ * @param[in] callback  pointer to an event handling function of two arguments
+ * @param[in] opaque    pointer to the function's intended 2nd argument
  * @return Nothing is returned.
  *
  */
